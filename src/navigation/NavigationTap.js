e086f13b019e15a68a982f1da3deda0b7c904153
Didier Irias <didier@firstfactory.com>
2023-08-05 16:24:36 -0600

    Adding icons

--- src/navigation/NavigationTap.js
+++ src/navigation/NavigationTap.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { Image } from 'react-native';
 import HomeScreen from "../screens/Home";
 import PhotoScreen from '../screens/Photo';
 import CameraScreen from "../screens/Camera";
@@ -8,9 +9,35 @@ const Tab = createBottomTabNavigator();
 export default function NavigationTab(){
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Camera" component={CameraScreen} />
-            <Tab.Screen name="Photo" component={PhotoScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} 
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                    source={focused ? require('../assets/home_icon.png') : require('../assets/home_icon.png')}
+                    style={{ width: 24, height: 24, tintColor: focused ? '#007AFF' : '#8E8E93' }}
+                    />
+                ),
+                }}/>
+            <Tab.Screen name="Camera" component={CameraScreen} 
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                    source={focused ? require('../assets/camera_icon.png') : require('../assets/camera_icon.png')}
+                    style={{ width: 24, height: 24, tintColor: focused ? '#007AFF' : '#8E8E93' }}
+                    />
+                ),
+                }}
+            />
+            <Tab.Screen name="Photo" component={PhotoScreen} 
+            options={{
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                    source={focused ? require('../assets/photo_icon.png') : require('../assets/photo_icon.png')}
+                    style={{ width: 24, height: 24, tintColor: focused ? '#007AFF' : '#8E8E93' }}
+                    />
+                ),
+                }}
+            />
         </Tab.Navigator>
     )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, Image, StyleSheet } from 'react-native';
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <Text>Home Menu</Text>
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../assets/logo.png")} style={styles.logo}/>
+      <Text style={styles.textFormat}>Versión beta 1.0.0</Text>
+      <Text style={styles.textFormat}>Didier Irias & Jeffrey Baltodano</Text>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#111111',
+    justifyContent: 'center', // Center content vertically
+    alignItems: 'center', // Center content horizontally
+  },
+  textFormat: {
+    color: '#ffffff',
+    marginTop: 10, // Add margin to separate text from logo
+    fontSize: 18, // Increase font size for better readability
+  },
+  logo: {
+    width: 250, // Adjust the width of the logo
+    height: 250, // Maintain the aspect ratio
+    marginBottom: 20, // Add margin at the bottom of the logo
+  },
+});
+
+
+
+
+
+
